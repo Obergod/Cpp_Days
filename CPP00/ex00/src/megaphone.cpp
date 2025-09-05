970c1b013e0dc17d0ae50ee6e0aea3c22e30f3f9
Mael Fioroni <mafioron@f2r5s18.clusters.42paris.fr>
2025-09-05 20:21:26 +0200

    ex00 done

---
+++ CPP00/ex00/src/megaphone.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mafioron <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/05 17:57:39 by mafioron          #+#    #+#             */
+/*   Updated: 2025/09/05 17:57:45 by mafioron         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac > 1)
+	{
+		for (int i = 1; i < ac; i++)
+			std::cout << av[i];
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
