826f1f87912260383853ae2efebcedceeae52d64
Mael Fioroni <mafioron@f1br6s15.clusters.42paris.fr>
2025-09-24 17:13:20 +0200

    ex04 finished

--- CPP01/ex04/src/main.cpp
+++ CPP01/ex04/src/main.cpp
@@ -12,13 +12,53 @@
 
 #include "replace.hpp"
 
-int	main()
+std::string	replace_all(const std::string &line, const std::string &from, const std::string &to)
 {
-	std::ifstream	inFile("texasdasdt.txt");
+	if (from.empty())
+		return (line);
+		
+
+	std::string n_line = line;
+	size_t		pos = 0;
+
+	while ((pos = n_line.find(from, pos)) != std::string::npos)
+	{
+		n_line.erase(pos, from.length());
+		n_line.insert(pos, to);
+		pos += to.length();
+	}
+	return (n_line);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac < 4)
+		return (1);
+
+	std::string		out_name = av[1];
+	std::string		s1 = av[2];
+	std::string		s2 = av[3];
+
+	out_name += ".replace";
+	std::ifstream	inFile(av[1]);
+	std::ofstream	outFile(out_name.c_str());
 	if (!inFile.is_open())
 	{
-		std::cout << "file does not exist" << std::endl;
+		std::cerr << "file does not exist" << std::endl;
 		return (1);
 	}
+	if (!outFile.is_open())
+	{
+		std::cerr << "outfile error" << std::endl;
+		return (1);
+	}
+
+	std::string	line;
+	while (getline(inFile, line))
+	{
+		std::string	n_line = replace_all(line, s1, s2);
+		outFile << n_line << std::endl;
+	}
+
 	return (0);
 }
