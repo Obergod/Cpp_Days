b0f3d19d49d96aff195a0f269bb2938211d859bf
Mael Fioroni <mafioron@f2r7s16.clusters.42paris.fr>
2025-11-13 21:05:34 +0100

    CPP 09 ex02 created

---
+++ CPP09/ex02/include/PmergeMe.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mafioron <mafioron@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/13 21:03:25 by mafioron          #+#    #+#             */
+/*   Updated: 2025/11/13 21:04:57 by mafioron         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <utility>
+#include <vector>
+#include <deque>
+
+class	Rpn
+{
+	private:
+		std::vector
+		std::deque
+	public:
+		Rpn();
+		Rpn(const Rpn &other);
+		Rpn &operator=(const Rpn &other);
+		~Rpn();
+
+
+//		class	DateTooEarlyException : public std::exception
+//		{
+//			public:
+//				virtual const char	*what() const throw();
+//		};
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
